3d245ddb9b14f2df5ee756930fb17e45d1e456ec
Alex Marrinan <alex@balroe.com>
2020-02-07 12:46:08 -0500

    Initial Commit

---
+++ 3D Brick BreakerTests/_D_Brick_BreakerTests.swift
@@ -0,0 +1,34 @@
+//
+//  _D_Brick_BreakerTests.swift
+//  3D Brick BreakerTests
+//
+//  Created by Alex Marrinan on 2/7/20.
+//  Copyright © 2020 Alex Marrinan. All rights reserved.
+//
+
+import XCTest
+@testable import _D_Brick_Breaker
+
+class _D_Brick_BreakerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
